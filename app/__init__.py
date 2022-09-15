711ec294aaa15750229260831d347e426e5adafc
Kogoon <gojs712@gmail.com>
2022-09-15 16:55:47 +0900

    22-09-15 Main page Commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import os
+import logging 
+import redis
+
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -10,19 +14,15 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
-    app.config['SECRET_KEY'] = '5db7c076eda442ce3aa59e1ce6384f14982de843644a90370'
-
-    #csrf  = CSRFProtect(app)
-
+    app.config['SECRET_KEY'] = 'testtesttest'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
     # ORM
     db.init_app(app)
     migrate.init_app(app, db)
     from . import models
 
-    # 블루프린트
-    from .views import main_views, auth_views, product_views
+    from .views import main_views
     app.register_blueprint(main_views.bp)
-    app.register_blueprint(auth_views.bp)
-    app.register_blueprint(product_views.bp)
 
     return app
