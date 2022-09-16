ec1fb33096989912fcc9849b217c7a1504336726
Hailey <80679085+S2Domingo@users.noreply.github.com>
2022-09-16 09:52:21 +0900

    Revert "22-09-15 Main page Commit"

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,3 @@
-import os
-import logging 
-import redis
-
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -14,15 +10,19 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
-    app.config['SECRET_KEY'] = 'testtesttest'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
+    app.config['SECRET_KEY'] = '5db7c076eda442ce3aa59e1ce6384f14982de843644a90370'
+
+    #csrf  = CSRFProtect(app)
+
     # ORM
     db.init_app(app)
     migrate.init_app(app, db)
     from . import models
 
-    from .views import main_views
+    # 블루프린트
+    from .views import main_views, auth_views, product_views
     app.register_blueprint(main_views.bp)
+    app.register_blueprint(auth_views.bp)
+    app.register_blueprint(product_views.bp)
 
     return app
